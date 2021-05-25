4754c9cfd08a36f3094e8de193f3bc90aad5f407
Sebastian Azcarate <seba.azc@gmail.com>
2021-05-25 19:01:41 -0300

    typo fixed

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Layout from "../components/Layout";
+import Layout from "../components/layout";
 import Head from "next/head";
 import Section from "../components/section";
 
